d6cab070389e086d0c17ba77ae7a4182e82ded1f
adai183 <andreas.daiminger@gmail.com>
2016-03-21 10:35:54 +0100

    refactor

--- app/containers/CreateMeetUpContainer.js
+++ app/containers/CreateMeetUpContainer.js
@@ -63,7 +63,7 @@ var CreateMeetUpContainer = React.createClass({
         lat: this.state.lat,
         lng: this.state.lng
       });
-      window.location= "/dist/map.html";
+      window.location= "/app/map.html";
     }
   },
   validateDates: function () {
